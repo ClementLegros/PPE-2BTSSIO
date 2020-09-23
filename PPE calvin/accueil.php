d9e5f37840d349a3e8ca59b00e430652b58ade0a
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2020-09-23 22:46:02 +0200

    eeze

--- PPE calvin/accueil.php
+++ PPE calvin/accueil.php
@@ -21,7 +21,7 @@ session_start();
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
     <!-- Navigation entre les pages du site -->
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleControls" class="caraccueil" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="images/montagnegris.jpg" class="d-block w-100" alt="...">
