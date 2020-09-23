c6d544fd36e72e1d42290baada7b66f9301524e0
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2020-09-23 23:56:49 +0200

    rien

--- PPE calvin/accueil.php
+++ PPE calvin/accueil.php
@@ -21,29 +21,29 @@ session_start();
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
     <!-- Navigation entre les pages du site -->
-    <div id="carouselExampleControls" class="caraccueil" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="images/montagnegris.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/montagneblanc.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <header>
+<div class="bg-warning py-2">
+	<div class="container">
+		Le port du masque est obligatoire sur l’ensemble des villages partenaires. #STOPCOVID19
+	</div>
 </div>
+<nav id="navbar_top" class="navbar navbar-expand-lg navbar-dark bg-primary">
+<div class="container">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="main_nav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+      <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+    </ul>
+  </div> <!-- navbar-collapse.// -->
+</div> <!-- container.// -->
+</nav>
+</header>
 
-  <img class="logoaccueil" src="images/logoremove.png" alt="">
+
 
 
 
