e0efbce4bfce6034b83bcab98d432b9c8c648271
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2020-09-24 09:33:22 +0200

    Changement carousel

--- PPE calvin/accueil.php
+++ PPE calvin/accueil.php
@@ -37,12 +37,45 @@ session_start();
     <ul class="navbar-nav ml-auto">
       <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
       <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+      <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
     </ul>
   </div> <!-- navbar-collapse.// -->
 </div> <!-- container.// -->
 </nav>
 </header>
 
+<!--carousel-->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="images/montagneblancredi.jpg" class="d-block w-100" alt="...">
+  </div>
+  <div class="carousel-item">
+    <img src="images/montagnejour.jpg" class="d-block w-100" alt="...">
+  </div>
+  <div class="carousel-item">
+    <img src="images/montagnegrisredi.jpg" class="d-block w-100" alt="...">
+  </div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+
+
+<!--activité-->
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 
 
 
