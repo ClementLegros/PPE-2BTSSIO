1feb06e1465c7fe30eea8691825840c6c5febc20
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2020-11-26 14:13:35 +0100

    Index

--- PPE calvin/index.php
+++ PPE calvin/index.php
@@ -11,18 +11,48 @@ session_start();
   </head>
   <body>
 
-    <nav class="navbar sticky-top navbar-light bg-light">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="navbar-brand" href="#">
-            <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" loading="lazy">
-          </a>
-        </li>
-      </ul>
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="navbar-brand" href="#">
-     </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">VVA</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php?index=accueil">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?index=animation">Animation</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Profil
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="index.php?index=profil">Profil</a>
+              <a class="dropdown-item" href="#">deconnexion</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <?php
+     switch ($_GET['index']) {
+       case 'accueil':
+         include("accueil.php");
+         break;
+       case 'animation':
+        include("animation.php");
+
+        break;
+        case 'profil':
+         include("profil.php");
+       break;
+     }
+
+     ?>
 
   </body>
 </html>
